81f08d44469379841caac48a917cefdb6dfdcd0e
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-05-22 01:08:30 -0300

    Update exercicio1.py

--- tipos_n_primitivos/exercicio1.py
+++ tipos_n_primitivos/exercicio1.py
@@ -1,1 +1,10 @@
 # Escreva um programa que some todos os itens de uma lista.
+#Vetor numbers
+numbers = []
+
+#Repetir a possibilidade de inserir números de acordo com o pedido do usuário
+for i in range(int(input("Vamos fazer uma soma? Quantos números você quer que eu some?: "))):
+    numbers.append(int(input("Digite um número: ")))
+
+#Entregar Soma
+print(f"A soma de {numbers} é: {sum(numbers)}")
