4962ebcf279b4a3ae4dc9cb216bb4f948475509c
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-05-25 12:37:49 -0300

    Update exercicio8.py

--- estruturas_de_repeticao/exercicio8.py
+++ estruturas_de_repeticao/exercicio8.py
@@ -1,1 +1,11 @@
-#Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato
+#Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre #pelo mais barato
+
+#Vetor numbers
+prices = []
+
+for i in range(int(input("Quantos produtos você quer analisar?: "))):
+#Pedir Número inteiro
+    prices.append(float(input(f"Digite o preço do produto: ")))
+
+#Indicar menor
+print(f"O mais barato dos produtos {prices} é: {min(prices)}")
