84008b8af53cdb9192f68c4533ab1cc961d0bf2c
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-05-25 12:19:46 -0300

    Update exercicio5.py

--- estruturas_de_repeticao/exercicio5.py
+++ estruturas_de_repeticao/exercicio5.py
@@ -1,1 +1,12 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
 
+
+#Vetor numbers
+numbers = []
+
+#Pedir Número inteiro
+numbers.append(int(input("Digite um numero: ")))
+#Pedir Número inteiro
+numbers.append(int(input("Digite outro número: ")))
+#Entregar maior
+print(f"O maior dos números {numbers} é: {max(numbers)}")
