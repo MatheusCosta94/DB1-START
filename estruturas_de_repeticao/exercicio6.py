d5f05ba4d2f64daa77c62999c2f680b342d36103
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-05-25 12:24:19 -0300

    Update exercicio6.py

--- estruturas_de_repeticao/exercicio6.py
+++ estruturas_de_repeticao/exercicio6.py
@@ -1,1 +1,12 @@
+#Faça um Programa que leia três números e mostre o maior deles.
 
+
+#Vetor numbers
+numbers = []
+
+for i in range(2):
+#Pedir Número inteiro
+    numbers.append(int(input("Digite um numero: ")))
+
+#Entregar maior
+print(f"O maior dos números {numbers} é: {max(numbers)}")
