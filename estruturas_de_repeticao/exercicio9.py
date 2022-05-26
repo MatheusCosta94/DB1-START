3d12b8d7c2a53175bbf482fc8a7329e2241ebf5c
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-05-25 23:37:13 -0300

    Update exercicio9.py

--- estruturas_de_repeticao/exercicio9.py
+++ estruturas_de_repeticao/exercicio9.py
@@ -1,1 +1,13 @@
 #Faça um Programa que leia três números e mostre-os em ordem decrescente.
+#Vetor numbers
+prices = []
+
+for i in range(int(input("Quantos produtos você quer analisar?: "))):
+#Pedir Número inteiro
+    prices.append(float(input(f"Digite o preço do produto: ")))
+    
+#Classificar e ordenadar de forma decrescente
+prices.sort(reverse = True)
+
+#Indicar menor
+print(prices)
