2afdf67194e76d86a45ad4da5b3e27a9f71581d1
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-03 23:00:32 -0300

    Update exercicios2.py

--- tipos_n_primitivos/exercicios2.py
+++ tipos_n_primitivos/exercicios2.py
@@ -1,5 +1,11 @@
 # Escreva um programa que multiplique todos os itens de uma lista.
+lista = []
+numbers = int(input('Quantos números você quer inserir na Lista?'))
+print (f'Ok, Então digite {numbers} números a medida que o sistema for solicitando!')
 
-numerolista = []
-numerolista.append(int(input('Inserir um valor: ')))
-print(numerolista)
+while len(lista) < numbers:
+    incluir = int(input('Digite um número: '))
+    lista.append(incluir)
+    
+print (f'Você formou uma lista composta por {lista}')
+print ('Que tal fazer uma tabuada com esse números?')
