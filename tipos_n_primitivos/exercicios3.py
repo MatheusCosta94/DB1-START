99e298b9fefa4a97eb7a3a1924ce76d36a6bfde6
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-16 21:55:25 -0300

    Update exercicios3.py

--- tipos_n_primitivos/exercicios3.py
+++ tipos_n_primitivos/exercicios3.py
@@ -1,1 +1,13 @@
 # Escreva um programa que retorne o maior e o menor número de uma lista.
+lista = []
+numbers = int(input('Quantos números você quer inserir na Lista?'))
+print (f'Ok, Então digite {numbers} números a medida que o sistema for solicitando!')
+
+while len(lista) < numbers:
+    incluir = int(input('Digite um número: '))
+    lista.append(incluir)
+
+menor = min(lista)
+maior = max(lista)
+
+print (f'O menor número da lista é {menor} e o maior é {maior}')
