3e35e5f88e77caa52c8cfd4bcd58afe12a23ea2e
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-29 23:27:35 -0300

    Create exercicio5.py

---
+++ tipos_n_primitivos/exercicio5.py
@@ -0,0 +1,4 @@
+# Escreva um programa que conte quantas string tenham tamanho maior que 2 e o primeiro e último caracteres sejam iguais. 
+#(Exemplo de lista: ['abc','xyz','aba','1221']
+
+lista = []
