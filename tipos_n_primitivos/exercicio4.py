ab5085c4d96e549604f93663a18d82ba6a2e3e2d
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-29 22:44:17 -0300

    Update exercicio4.py

--- tipos_n_primitivos/exercicio4.py
+++ tipos_n_primitivos/exercicio4.py
@@ -1,26 +1,5 @@
 # Escreva um programa que conte o número de caracteres de uma string ( Exemplo: 'google.com' Resultado Esperado: {'o': 3,'g': 2,'.': 1,'e': 1,'l': 1,'m': 1,'c': 1} )
-def removeDuplicates(frase):
-    chars = []
-    prev = None
- 
-    for c in frase:
-        if prev != c:
-            chars.append(c)
-            prev = c
- 
-    return ''.join(chars)
- 
- 
-if __name__ == '__main__':
- 
-    frase = 'google'
-    
-    
-newlist = list(removeDuplicates(frase))
-listcont = []
+from collections import Counter
 
-while len(frase) < max(frase):
-    incluir = ('a')
-    listcont.append(incluir)
-
-print(listcont)
+frase = input("Digite uma frase: ")
+print (Counter(frase))
