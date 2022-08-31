fc1502f7542549bbeeb963dd18ca85ca2fe8aa2a
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-30 23:18:36 -0300

    Update exercicio5.py

--- tipos_n_primitivos/exercicio5.py
+++ tipos_n_primitivos/exercicio5.py
@@ -1,6 +1,8 @@
 # Escreva um programa que conte quantas string tenham tamanho maior que 2 e o primeiro e último caracteres sejam iguais. 
 #(Exemplo de lista: ['abc','xyz','aba','1221']
 
+
+from collections import Counter
 lista = []
 
 contad = int(input('Vamos fazer uma lista de strings, quantas palavras você quer incluri na lista:'))
@@ -9,8 +11,9 @@ for i in range(contad):
     frase = input(f'Digite a palavra {i}:')
     lista.append(frase)
 
-contadfinal = ()
-
+contador = (0)
 for i in range(contad):
-    result = lista[0]
-print (result)
+    separar_strings = list(lista[i])
+    if len(lista[i]) > 2 and separar_strings[0] == separar_strings[-1]:
+        contador += 1
+print(contador)
