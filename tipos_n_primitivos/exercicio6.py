2ec1b1290ec1358ee072d0c21c285c40db6b3a0a
Matheus Costa <43269848+MatheusCosta94@users.noreply.github.com>
2022-08-30 23:32:22 -0300

    Create exercicio6.py

---
+++ tipos_n_primitivos/exercicio6.py
@@ -0,0 +1,3 @@
+# Escreva um programa que ordene em ordem crescente uma lista de tuplas informadas, pelo último item da
+# tupla (Exemplo de lista: [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Resultado esperado: [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)] )
